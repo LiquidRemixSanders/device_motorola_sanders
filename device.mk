d81228d21e4928af3edb8678190f47ace8836263
Syed Sajid <syedsajid341@gmail.com>
2018-10-05 18:09:06 +0000

    sanders:Switch to common PowerHAL instead of lineage

--- device.mk
+++ device.mk
@@ -295,7 +295,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
