3dc137d20e62acb22e8a84b58e5b0a275b3562b4
Syed Sajid <syedsajid341@gmail.com>
2018-10-16 19:37:44 +0000

    venomos bringup

--- liquid.mk
+++ liquid.mk
@@ -41,7 +41,7 @@ TARGET_DEVICE=liquid_sander PRODUCT_NAME="Moto G5s Plus"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Syed Sajid (syedsajid)"
-export LIQUID_BUILD_TYPE=RELEASE
+export LIQUID_BUILDTYPE=RELEASE
 
 # for specific
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
