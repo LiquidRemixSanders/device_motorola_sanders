4790b9254c1ebce555ebfe34f291dea71d975ae6
Syed Sajid <syedsajid341@gmail.com>
2018-10-28 18:58:28 +0000

    Set BootAnimation_res

--- pixys_sanders.mk
+++ pixys_sanders.mk
@@ -26,8 +26,7 @@ $(call inherit-product, device/motorola/sanders/device.mk)
 $(call inherit-product, vendor/pixys/config/common.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
