20d8bba13c2da924862fc514fb038f0eb0fa0eab
Syed Sajid <syedsajid341@gmail.com>
2018-11-11 20:47:44 +0000

    Revert "Revert "sanders: Move fingerprint back to makefile""
    This reverts commit feabbca8ddf68a53c9774e507c09cd409741041c.

--- liquid.mk
+++ liquid.mk
@@ -40,8 +40,9 @@ PRODUCT_BUILD_PROP_OVERRIDES +=
 TARGET_DEVICE=liquid_sander PRODUCT_NAME="Moto G5s Plus"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Syed Sajid (syedsajid)"
-export "LIQUID_BUILDTYPE=RELEASE"
+    DEVICE_MAINTAINERS="Syed Sajid (syedsajid)" \
+    PRIVATE_BUILD_DESC="sanders-7.1.1/NPS26.116-26/30:user/release-keys"
+export LIQUID_BUILD_TYPE=RELEASE
 
 # for specific
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
