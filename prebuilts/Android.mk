00154589fa196ca811741c9cb90c4786b6bebb0d
Syed Sajid <syedsajid341@gmail.com>
2018-11-12 13:41:21 +0000

    prebuilts; reomve duplicates

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -1,17 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := PixelLauncher3
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/PixelLauncher/PixelLauncher3.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
-LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3 Launcher3QuickStep PixelLauncher
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleCamera
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := priv-app/googlecamera/GoogleCamera.apk
