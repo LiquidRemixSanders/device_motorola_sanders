92169a5177a9efb5941865b6b0f14ed6fdce0c88
syed sajid <syedsajid341@gmail.com>
2018-12-04 20:34:31 +0000

    Blissfy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/liquid.mk
+	$(LOCAL_DIR)/bliss_sanders.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo liquid_sanders-userdebug
-add_lunch_combo liquid_sanders-eng
+add_lunch_combo bliss_sanders-userdebug
+add_lunch_combo bliss_sanders-eng
