99b00005828810d8588c15d7dcd45d31e28714aa
Sajidjnl <syedsajid341@gmail.com>
2019-02-16 19:13:38 +0100

    sanders:remove prebuilts no additional apps use defsult camera only

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    GoogleCamera \
+    Camera2 \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
