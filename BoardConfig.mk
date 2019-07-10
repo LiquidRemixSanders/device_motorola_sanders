940a99025f931bbfd32e75748d05f806d1fa073f
Sajidjnl <syedsajid341@gmail.com>
2019-07-10 16:25:59 +0000

    Sander:Set Neverallow flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,6 +229,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
